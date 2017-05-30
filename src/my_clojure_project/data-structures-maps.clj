764e2610f1aafb1350494c95c2595ae32839168a
bwoodfork <byronwoodfork@gmail.com>
2017-05-30 11:53:59 -0700

    adds notes for data-structures-maps

--- src/my_clojure_project/data-structures-maps.clj
+++ src/my_clojure_project/data-structures-maps.clj
@@ -1,3 +1,31 @@
 (ns my-clojure-project.data-structures-maps)
 
-; * Maps
+; * Maps - Another form of a collection
+; ----------------------------------------------------------------------
+
+; Can be empty
+{}
+
+; Hold a collection of key-value pairs of data
+
+; key        value
+{:first-name "Harry"}
+
+; Can hold multiple key-value pairs
+{:first-name "Harry" :last-name "Potter"}
+
+; Values can also be other, nested maps
+{:names {:first-name "Ron" :last-name "Weasley"}}
+
+; Can be created multiple ways
+; - With a literal
+{:first-name "Harry"}
+
+; - With a function call
+(hash-map :first-name "Harry" :last-name "Potter")
+
+; - Can have key-value pairs added (or associated) with them, creating a new map
+(assoc {:first-name "Harry"} :last-name "Potter")
+
+; - Can also have key-value pairs removed
+(dissoc {:first-name "Harry"} :last-name)
